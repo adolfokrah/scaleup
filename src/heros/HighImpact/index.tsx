ae5ae124e8b5f373a85416b0ffe72df614c6b2d2
adolfokrah <adolfokrah@gmail.com>
2025-09-01 10:57:00 +0200

    fix:

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -68,7 +68,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
       </div>
       <div className="w-full h-full bg-black/40 absolute left-0 top-0">fdfs</div>
 
-      <div className=" min-h-[60vh] md:min-h-[78vh] select-none">
+      <div className=" min-h-[60vh] md:min-h-[85vh] select-none">
         {media && typeof media === 'object' && (
           <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
         )}
